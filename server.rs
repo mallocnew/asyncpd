529ede963b5ac97eaf4b274bc7af9ea50d12f0d6
jok <easytojoin@163.com>
2024-04-16 13:51:39 +0800

    rust: http server

---
+++ server.rs
@@ -0,0 +1,26 @@
+// Copyright 2024 GOTHAM Inc. All Rights Reserved.
+// Author: easytojoin@163.com (jok)
+use std::net::SocketAddr;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Listening on: http://{}", addr);
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+            let _ = stream.read(&mut buffer).await;
+
+            let contents = "<h1>Hello, world!</h1>";
+            let content_length = contents.len();
+            let response =
+                format!("HTTP/1.1 200 OK\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+            let _ = stream.write_all(response.as_bytes()).await;
+        });
+    }
+}
